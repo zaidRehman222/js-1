2ab11bf6f2a4268ad75e34c4c62d23f2fad0b774
Zaid Rehman khan <zaidurrehman452@gmail.com>
2025-12-12 23:54:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,121 @@
+// -----------Chapter no 01-------------
+// Question no 01
+
+// alert()
+
+// Question no 02
+
+// alert("Hello")
+
+// Question no 03
+
+// alert("Saylani Welfair \n International Trust")
+
+// Question no 04
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// Question no 05
+
+// var fullname = "Hello... I can run JS through my web browser console"
+// console.log(fullname)
+
+// -----------Chapter no 02-------------
+
+// Question no 01
+
+// var username = "Hunain Khan"
+
+// Question no 02
+
+// var myName = "Hunain "
+// var fullname = "Hunain Khan"
+
+// Question no 03
+
+// var message = ("Hello World")
+// alert (message)
+
+// Question no 04
+
+// var name1 = "PIZZA"
+// var name2 = "PIZZ"
+// var name3 = "PIZ"
+// var name4 = "PI"
+// var name5 = "P"
+// alert (name1 + "\n" + name2+ "\n" + name3+ "\n" + name4+ "\n" + name5)
+
+// Question no 05
+
+// var Email = ("abc@Email.com")
+// alert (Email)
+
+// Question no 06
+
+// var book = ("A smarter way to learn js")
+// alert("I am trying to learn from the Book" + book)
+
+// Question no 07
+
+// var fullname = "Yeh! I can write HTML content through JS"
+// document.write("<h1>" + fullname + "</h1>")
+
+// -----------Chapter no 03-------------
+
+// Question no 01
+
+// var age = ("I am 22 years old")
+// alert (age)
+
+// Question no 02
+
+// var title = ("You have visible this site 14 times")
+// alert (title)
+
+// Question no 03
+
+// var fullname = "My birth year is 2003 <br> Data type of my declared variable is number"
+// document.write("<p>" + fullname + "</p>")
+
+// Question no 04
+
+// var visitor_name =("John Doe")
+// var order =("orderded")
+// var on =("on")
+// var Product_title = ("5 T-shirt(s)")
+// var store = ("XYZ Clothing store")
+// document.write ("<b>" + visitor_name + "</b>" + " " + order + " " + "<b>" + Product_title +"</b>" + " " + on + " " +  "<b>" + store + "</b>")
+
+// -----------Chapter no 04-------------
+
+// Question no 01
+
+// var heading = ("Rules for naming JS variable")
+// var pra1 = ("Variable names can only contain , number , $ and _ . For example : $my_1stVariable")
+// var pra2 = ("Variable names begin with a letter , $ or _ . For example : _name or name")
+// var pra3 = ("Variable names are case sensitive")
+// var pra4 = ("Variable names should not be JS keywords")
+// document.write = ("<h1>" + heading + "</h1>"  + "br" + "<p>" + pra1 + "</p>" + "br" + "<p>" + pra2 + "</p>" + "br" + "<p>" + pra3 + "</p>" + "br" + "<p>" + pra4 + "</p>")
+
+// Question no 01
+
+// -------------legal----------------
+
+// var user_name;
+// var $user_name;
+// var username;
+// var userName;      saylaniWelfairInternationalTrust
+// var user_name;
+// var userName01      
+
+//--------------illigal--------------
+
+// var 1username; 
+// var USERNAME ="hamza";
+// var username 
+// console.log(USERNAME);
+// console.log(username)
+// var user name = "hunain"
+// var user@Name ="khan"
+// var var ="value"; 
